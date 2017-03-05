13a442ee392414db18ca75f08eaf30923e790d81
weirdbrume <weirdbrume@gmail.com>
2017-03-05 20:00:52 +0500

    update hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ def app(environ, start_response):
     headers = [('Content-Type', 'text/plain')]
     params = [bytes(param, encoding='utf-8') for param in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
-    return [params]
+    return iter([params])
