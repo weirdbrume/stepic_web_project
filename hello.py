baea33e8f9bcf571d08322d413c0e475390ccb9a
weirdbrume <weirdbrume@gmail.com>
2017-03-05 19:14:03 +0500

    update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    params = [param + '/n' for param in environ['QUERY_STRING'].split('&')]
+    params = [param for param in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
     return [params]
