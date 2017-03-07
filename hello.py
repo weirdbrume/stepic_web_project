da76e1f5be8e1e94c22c7bcb4074b28de87d5579
weirdbrume <weirdbrume@gmail.com>
2017-03-07 20:31:33 +0500

    update hello.py and init.sh

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    params = [bytes(param, encoding='utf-8') for param in environ['QUERY_STRING'].split('&')]
+    params = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
     start_response(status, headers)
-    return iter([params])
+    return params
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
