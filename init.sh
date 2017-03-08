94fd37a18b00bece6055d0d365f58c9fbddc1e1a
weirdbrume <weirdbrume@gmail.com>
2017-03-08 20:24:34 +0500

    change gunicorn.conf and update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn --bind=0.0.0.0:8080 hello:app
