5b69875cf4ada16ab0df4704b10973016dfe0c3f
weirdbrume <weirdbrume@gmail.com>
2017-03-17 18:33:38 +0500

    update init.sh

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo gunicorn --bind=0.0.0.0:8080 hello:app
-sudo bash /ask/gunicorn_start.sh
+sudo bash home/box/web/ask/gunicorn_start.sh
