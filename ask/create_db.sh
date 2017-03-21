848a69786dfc30a42df826086ba95e59c0a8c5fd
weirdbrume <weirdbrume@gmail.com>
2017-03-21 20:35:50 +0500

    create "create_db.sh" and update "init.sh"

---
+++ ask/create_db.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo mysql -u root -e "create database if not exists db"
--- init.sh
+++ init.sh
@@ -3,7 +3,10 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+sudo /etc/init.d/mysql start
+
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo gunicorn --bind=0.0.0.0:8080 hello:app
 cd /home/box/web/ask
+sudo bash /home/box/web/ask/create_db.sh
 sudo bash /home/box/web/ask/gunicorn_start.sh
