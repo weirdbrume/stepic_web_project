f158e0cf09acf486bd1343b07683cd91af43d8b2
weirdbrume <weirdbrume@gmail.com>
2017-03-22 18:05:24 +0500

    update "create_db.sh"

--- ask/create_db.sh
+++ ask/create_db.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-sudo mysql -u root -e "create database if not exists db default character set utf8 collate utf8_general_ci;"
+sudo mysql -u root -e "create database if not exists box_db default character set utf8 collate utf8_general_ci;
+create user 'box'@'localhost' identified by 'password';
+grant all privileges on box_db . * to 'box'@'localhost';
+flush privileges;"
