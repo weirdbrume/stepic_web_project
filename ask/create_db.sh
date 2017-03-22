7eea99ebc0033128bc0783aee5ba26ab3a2f7fcb
weirdbrume <weirdbrume@gmail.com>
2017-03-22 17:04:54 +0500

    update "create_db.sh"

--- ask/create_db.sh
+++ ask/create_db.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo mysql -u root -e "create database if not exists db"
+sudo mysql -u root -e "create database if not exists db default character set utf8 collate utf8_general_ci;"
