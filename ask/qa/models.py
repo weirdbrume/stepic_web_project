c7cb4fcda4f691ae68368c53c0547a9e0493a41f
weirdbrume <weirdbrume@gmail.com>
2017-03-22 20:29:02 +0500

    remade qa app

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
+
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
