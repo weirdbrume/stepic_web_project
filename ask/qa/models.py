2c0402f3917aee75e97d65e6de981cec4c47f9e3
weirdbrume <weirdbrume@gmail.com>
2017-03-23 17:20:47 +0500

    update models.py in qa

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,14 +7,14 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField()
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_likes')
 
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
+    added_at = models.DateField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
