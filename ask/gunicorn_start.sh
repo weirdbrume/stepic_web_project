db0c82ea74be1b34a300b7702756724b74f38d30
weirdbrume <weirdbrume@gmail.com>
2017-03-23 15:02:26 +0500

    update init.sh

--- ask/gunicorn_start.sh
+++ ask/gunicorn_start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+# sudo gunicorn --bind=0.0.0.0:8080 hello:app
 sudo gunicorn ask.wsgi:application --bind 0.0.0.0:8000
--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo /etc/init.d/mysql start
 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-# sudo gunicorn --bind=0.0.0.0:8080 hello:app
 cd /home/box/web/ask
 sudo bash /home/box/web/ask/create_db.sh
+sudo python3 manage.py makemigrations qa
+sudo python3 manage.py migrate qa
+
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo bash /home/box/web/ask/gunicorn_start.sh
